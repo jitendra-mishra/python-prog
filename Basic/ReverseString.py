1d4e4d9009597e8dc8412265dfec9557237394f8
Jitendra <jitendra.mishra@gmail.com>
2020-10-06 22:38:56 +0530

    method2

--- Basic/ReverseString.py
+++ Basic/ReverseString.py
@@ -9,6 +9,13 @@ def reversedString(str):
         temp_str+=str[l-i-1]
     return temp_str
 
+#method 2 better way
+def reverseString2(str):
+    temp_str = ""
+    for c in str:
+        temp_str = c + temp_str
+    return temp_str
+
 print("Original str=", my_str)
-my_str = reversedString(my_str)
+my_str = reverseString2(my_str)
 print("Reversed str=", my_str)
