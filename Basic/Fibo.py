bccd2fa08cd95ee2c5bd783904f960f4733ea466
Jitendra <jitendra.mishra@gmail.com>
2020-10-13 20:13:30 +0530

    fibo

--- Basic/Fibo.py
+++ Basic/Fibo.py
@@ -29,3 +29,21 @@ else:
         f1 = sum
         print(f1,  end=" ")
 
+print()
+print()
+#fibonacci series
+def fibo3(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
+
+fibo3(4)
